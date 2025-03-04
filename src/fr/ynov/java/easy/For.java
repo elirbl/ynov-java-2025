002670eb5e6180dbf356d1b13571c7dcf0ebd5c9
Robl Elisabeth <elisabeth.robl@ynov.com>
2025-03-04 16:24:45 +0100

    For

---
+++ src/fr/ynov/java/easy/For.java
@@ -0,0 +1,9 @@
+package fr.ynov.java.easy;
+
+public class For {
+    public static void main(String[] args) {
+        for (int x = 1; x <= 10; x++) {
+            System.out.println(x);
+        }
+    }
+}
