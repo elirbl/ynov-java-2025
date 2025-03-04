78c0a76dce84e65ea85a20f67bb7ed4c737ef02f
Robl Elisabeth <elisabeth.robl@ynov.com>
2025-03-04 16:54:43 +0100

    Arrays with for

--- src/fr/ynov/java/easy/Arrays.java
+++ src/fr/ynov/java/easy/Arrays.java
@@ -3,6 +3,9 @@ package fr.ynov.java.easy;
 public class Arrays {
     public static void main(String[] args) {
         int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-        System.out.println(java.util.Arrays.toString(arr));
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
     }
 }
---
+++ src/fr/ynov/java/easy/ScanMe.java
@@ -0,0 +1,12 @@
+package fr.ynov.java.easy;
+
+import java.util.Scanner;
+
+public class ScanMe {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of the array: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+    }
+}
